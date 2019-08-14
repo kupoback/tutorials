19dca6cb038029c8f5fd17dde55d0f684156ee2f
Nick Makris <>
2019-08-13 19:18:21 -0500

    Converted to php for easier management - 2

--- vuejs/index.php
+++ vuejs/index.php
@@ -1,9 +1,10 @@
 <?php
 $title = 'Vue Playground';
+$header = 'A listing of projects I\'m learning/working on';
 include('partials/head.php');
 
 include('partials/nav.php');
 
-include('partials/content-header.php');
+include('partials/front-content-header.php');
 
 include('partials/footer.php');
